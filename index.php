d21dbd567dbfdb0a49f0d9f07e80a53528dd4bb7
icewize <florian.charlot53@gmail.com>
2022-09-20 14:12:02 +0200

    Ca marche

--- index.php
+++ index.php
@@ -5,3 +5,6 @@ ini_set('display_errors', 'on');
 $datetime = new DateTime();
 $datetime->setTimezone(new DateTimeZone('Europe/Paris'));
 echo $datetime->format('d-m-Y H:i:s');
+
+
+echo "On est trop bon!!!!";
