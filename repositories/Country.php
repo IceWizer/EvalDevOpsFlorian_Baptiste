79d9c8db9abf3205843e84159e44a03c8e464c09
icewize <florian.charlot53@gmail.com>
2022-09-20 16:53:57 +0200

    Re

--- repositories/Country.php
+++ repositories/Country.php
@@ -7,71 +7,77 @@ require_once dirname(__FILE__) . '/Entity.php';
  *
  * @author Florian
  */
-class Country extends Entity{
-    private $label;
-
-    public function __construct($p_identifier, $p_label)
+namespace Entity
+{
+    class Country extends Entity
     {
-        parent::__construct($p_identifier);
+        private $label;
 
-        $this->label = $p_label;
-    }
-    public static function __constructStatic($p_identifier, $p_label)
-    {
-        return new Country($p_identifier, $p_label);
-    }
-
-    public function getLabel()
-    {
-        return $this->label;
-    }
-
-    public static function insert($label)
-    {
-        
-        $sql = 'INSERT INTO Country (label) VALUES (:label)';
-        
-        MyPDO::InsertDelete($sql, array(':label' => $label));
-    }
-
-    public function view()
-    {
-        echo "<td>" . htmlspecialchars($this->identifier) . "</td>";
-        echo "<td>" . htmlspecialchars($this->label) . "</td>";
-        echo "<td class=\"col-3\">";
-        echo '<a class="btn btn-info" href="./country.php?identifier=' . $this->identifier . '">Détails</a>';
-        echo "</td>";
-    }
-
-    public static function getCountries()
-    {
-        $sql = 'SELECT Country.identifier, Country.label
-                FROM Country';
-        
-        $countries = array();
-        
-        $countriesRaw = MyPDO::Select($sql, array());
-        $countriesFetch = $countriesRaw->FetchAll(PDO::FETCH_FUNC, "country::__constructStatic");
-        
-        foreach ($countriesFetch as $country)
+        public function __construct($p_identifier, $p_label)
         {
-            $countries[] = $country;
-        }
-        
-        return $countries;
-    }
+            parent::__construct($p_identifier);
 
-    public function jsonSerialize(): array {
-        return [
-            'identifier' => $this->identifier,
-            'label' => $this->label
-                ];
-    }
-    
-    public function toArray(): array {
-        return [
-            'identifier' => $this->identifier,
-            'label' => $this->label
-                ];
+            $this->label = $p_label;
+        }
+
+        public static function __constructStatic($p_identifier, $p_label)
+        {
+            return new Country($p_identifier, $p_label);
+        }
+
+        public function getLabel()
+        {
+            return $this->label;
+        }
+
+        public static function insert($label)
+        {
+            $sql = 'INSERT INTO Country (label) VALUES (:label)';
+
+            \myPDO\MyPDO::InsertDelete($sql, array(':label' => $label));
+        }
+
+        public function view()
+        {
+            echo "<td>" . htmlspecialchars($this->identifier) . "</td>";
+            echo "<td>" . htmlspecialchars($this->label) . "</td>";
+            echo "<td class=\"col-3\">";
+            echo '<a class="btn btn-info" href="./country.php?identifier=' . $this->identifier . '">Détails</a>';
+            echo "</td>";
+        }
+
+        public static function getCountries()
+        {
+            $sql = 'SELECT Country.identifier, Country.label
+                    FROM Country';
+
+            $countries = array();
+
+            $countriesRaw = \myPDO\MyPDO::Select($sql, array());
+            $countriesFetch = $countriesRaw->FetchAll(\PDO::FETCH_FUNC, "country::__constructStatic");
+
+            foreach ($countriesFetch as $country)
+            {
+                $countries[] = $country;
+            }
+
+            return $countries;
+        }
+
+        public function jsonSerialize(): array
+        {
+            return [
+                'identifier' => $this->identifier,
+                'label' => $this->label
+                    ];
+        }
+ 
+        public function toArray(): array
+        {
+            return [
+                'identifier' => $this->identifier,
+                'label' => $this->label
+                    ];
+        }
     }
 }
