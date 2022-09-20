5018c1b86965adf55fd49d40e58495633c73058c
icewize <florian.charlot53@gmail.com>
2022-09-20 15:07:14 +0200

    Re

--- index.php
+++ index.php
@@ -5,11 +5,5 @@ ini_set('display_errors', 'on');
 $datetime = new DateTime();
 $datetime->setTimezone(new DateTimeZone('Europe/Paris'));
 echo $datetime->format('d-m-Y H:i:s') . "<br/>";
-echo $datetime->format('d-m-Y H:i:s') . "<br/>";
-
 
 echo "On est trop bon!!!! <br/>";
-echo "Re <br/>";
-echo "Re <br/>";
-echo "Re <br/>";
-echo "Re <br/>";
