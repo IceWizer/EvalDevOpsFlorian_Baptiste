356129d8d130934449d8e13ecf571d18f0049684
icewize <florian.charlot53@gmail.com>
2022-09-20 16:19:26 +0200

    Re

--- lib/database/MyPDO.php
+++ lib/database/MyPDO.php
@@ -5,6 +5,7 @@
  *
  * @author Florian
  */
+
 namespace myPdo
 {
     abstract class MyPDO 
@@ -12,21 +13,21 @@ namespace myPdo
         private static $databaseName = 'mysql:dbname=cityData;host=localhost';
         private static $databaseUser = 'root';
         private static $databasePassword = 'Not24get';
-        
+
         public static function select($sql, $parameters = null): \PDOStatement 
         {
             $database = new \PDO(self::$databaseName, self::$databaseUser, self::$databasePassword);
-            
+
             $sth = $database->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
             $sth->execute($parameters);
-            
+
             return $sth;
         }
-        
+
         public static function insertDelete($sql, $parameters = null) 
         {
             $database = new \PDO(self::$databaseName, self::$databaseUser, self::$databasePassword);
-            
+
             $sth = $database->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
             $sth->execute($parameters);
         }
